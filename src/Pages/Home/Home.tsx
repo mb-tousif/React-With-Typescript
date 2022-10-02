7ebc2a4853db4579bc631e56e93c9781502ddf39
AZIZUL HAQUE TOUSIF <meta.block.tousif@gmail.com>
2022-10-03 00:57:07 +0600

    first commit

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+      <div className="text-center p-28">
+        <h1 className="text-3xl text-lime-800">
+          React with Typescript, <br />
+          tailwind CSS and
+          <br /> React Router Dom <br /> Initial Setup
+        </h1>
+      </div>
+    );
+};
+
+export default Home;
